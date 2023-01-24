0589a24223405c0cb23a33a0f5c21542de8c19dc
Sangamg145 <55407499+Sangamg145@users.noreply.github.com>
2023-01-24 11:38:50 +0530

    add contact us route

---
+++ routes/contactUs.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const nodemailer = require('nodemailer');
+
+router.post("/contact-us", async (req, res) => {
+  try {
+    // const email = await User.findOne({ email: req.body.email });
+    // email && res.status(200).json("User already exists");
+    // const newUser = new User({
+    //   name: req.body.username,
+    //   password: cryptoJS.AES.encrypt(
+    //     req.body.password,
+    //     process.env.PASS_KEY
+    //   ).toString(),
+    //   email: req.body.email,
+    // });
+    // const saveUser = await newUser.save();
+    // res.status(201).json(saveUser);
+
+
+let mailTransporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',
+    port:587,
+    requireTLS:'true',
+    secure:false,
+	auth: {
+		user: 'datandler1@gmail.com',
+		pass: 'pjtnvsbxapjxdspm'
+	}
+});
+
+let mailDetails = {
+	from: 'datandler1@gmail.com',
+	to: req.body.email,
+	subject: 'Datandler Software',
+	text: req.body.message
+};
+
+mailTransporter.sendMail(mailDetails, function(err, data) {
+	if(err) {
+		console.log('Error Occurs',err);
+	} else {
+		console.log('Email sent successfully');
+	}
+});
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
